29828124618f9a31231767fe45f5fb4d5b17973d
Alex Matos <alex.matos.freitas@gmail.com>
2022-01-13 17:35:03 -0300

    improve register screen

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
-import { useForm } from 'react-hook-form';
+
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
+import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 
 import { InputForm } from '../../components/Form/InputForm';
 import { Button } from '../../components/Form/Button';
@@ -42,10 +47,13 @@ export function Register() {
 		label: 'Categoria',
 	});
 
+	const navigation = useNavigation();
+
 	const {
 		control,
 		handleSubmit,
 		formState: { errors },
+		reset,
 	} = useForm({
 		resolver: yupResolver(schema),
 	});
@@ -62,7 +70,7 @@ export function Register() {
 		setCategoryModalOpen(false);
 	}
 
-	function handleRegister(form: FormData) {
+	async function handleRegister(form: FormData) {
 		if (!transactionType) {
 			return Alert.alert('Selecione o tipo de transação');
 		}
@@ -71,14 +79,36 @@ export function Register() {
 			return Alert.alert('Selecione uma categoria');
 		}
 
-		const data = {
+		const newTransaction = {
+			id: String(uuid.v4()),
 			name: form.name,
 			amount: form.amount,
-			transactionType,
+			type: transactionType,
 			category: category.key,
+			date: new Date(),
 		};
 
-		console.log(data);
+		try {
+			const dataKey = '@gofinances:transactions';
+			const data = await AsyncStorage.getItem(dataKey);
+			const currentData = data ? JSON.parse(data) : [];
+
+			const dataFormatted = [...currentData, newTransaction];
+
+			await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+			reset();
+			setTransactionType('');
+			setCategory({
+				key: 'category',
+				label: 'Categoria',
+			});
+
+			navigation.navigate('Listagem');
+		} catch (error) {
+			console.log(error);
+			Alert.alert('Não foi possível salvar a transação');
+		}
 	}
 
 	return (
