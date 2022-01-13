83e64a7e9e1595ee5eff6d5717c8aefa17454834
Alex Matos <alex.matos.freitas@gmail.com>
2022-01-12 23:26:52 -0300

    add categories file

---
+++ src/utils/categories.ts
@@ -0,0 +1,38 @@
+export const categories = [
+	{
+		key: 'purcheses',
+		label: 'Compras',
+		icon: 'shopping-bag',
+		color: '#5636D3',
+	},
+	{
+		key: 'food',
+		label: 'Alimentação',
+		icon: 'coffee',
+		color: '#FF872C',
+	},
+	{
+		key: 'salary',
+		label: 'Salário',
+		icon: 'dollar-sign',
+		color: '#12a454',
+	},
+	{
+		key: 'car',
+		label: 'Carro',
+		icon: 'crosshair',
+		color: '#E83F5B',
+	},
+	{
+		key: 'leisure',
+		label: 'Lazer',
+		icon: 'heart',
+		color: '#26195C',
+	},
+	{
+		key: 'studies',
+		label: 'Estudos',
+		icon: 'book',
+		color: '#9C001A',
+	},
+];
